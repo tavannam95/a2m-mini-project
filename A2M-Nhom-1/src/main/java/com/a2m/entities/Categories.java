5a5f0149af0443cf5f4567b30bd92bd5612ff637
tavannam95 <89097530+tavannam95@users.noreply.github.com>
2023-03-07 10:48:23 +0700

    namtv update pom

---
+++ A2M-Nhom-1/src/main/java/com/a2m/entities/Categories.java
@@ -0,0 +1,11 @@
+package com.a2m.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+public class Categories {
+    @Id
+    private Integer id;
+}
